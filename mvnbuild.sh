c18d9cf11c411334630508adf34a8851a5c81ffe
anyone <anyone@ao.com>
2024-06-12 11:08:32 +0800

    no message

--- mvnbuild.sh
+++ mvnbuild.sh
@@ -2,4 +2,5 @@ mvn clean package site
 mvn install:install-file -Dfile=./xalan/lib/xpath31_types.jar -DgroupId=xml.xpath31 -DartifactId=xpath31_types -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=./xalan/lib/json-20200518.jar -DgroupId=org.json -DartifactId=json-20200518 -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=./xalan/lib/endorsed/xercesImpl.jar -DgroupId=org.apache -DartifactId=xercesImpl -Dversion=2.12.3 -Dpackaging=jar
+mvn install:install-file -Dfile=./xalan/lib/endorsed/xml-apis.jar -DgroupId=org.apache -DartifactId=xml-apis -Dversion=1.4.02 -Dpackaging=jar
 
